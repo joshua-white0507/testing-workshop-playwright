e805abca2105d53b51f116dee3d6ecfed580b140
joshua-white0507 <joshua.white@solirius.com>
2025-03-25 09:54:58 +0000

    find calcualte holiday entitlement

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,24 @@
 import {expect, test} from "@playwright/test";
 
-test(`First test`, async ({ page }) => {
-    await page.goto('');
+test(`Find Calculate Holiday Entitlement`, async ({ page }) => {
+    await page.goto('https://www.gov.uk/calculate-your-holiday-entitlement');
     await expect(page.locator(`.govuk-heading-xl`))
         .toHaveText(`Calculate holiday entitlement`);
-    // Continue me! Yes will do
+    
+    // Continue me!
     // How would you click the continue button?
 });
+
+
+
+test(`Click start button`, async ({ page }) => {
+    await page.goto('https://www.gov.uk/calculate-your-holiday-entitlement');
+    const locator = page.getByRole('button', { name: 'form_start' });
+
+    await locator.hover();
+    await locator.click();
+    
+    // Continue me!
+    // How would you click the continue button?
+});
+
